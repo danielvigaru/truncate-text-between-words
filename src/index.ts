d15d499d929c84de6a6f3136e1ef37b8db534001
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2024-05-06 14:32:57 +0300

    fix jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * Returns the position of the last space in the string that's before the given index
- * @param text {String} The text that will be searched
- * @param index {Number} Searches for last space before this index
+ * @param {String} text The text that will be searched
+ * @param {Number} index Searches for last space before this index
  * @returns {Number} The position of the space
  */
 function getPositionOfLastSpaceBeforeIndex(text: string, index: number): number {
@@ -12,6 +12,7 @@ function getPositionOfLastSpaceBeforeIndex(text: string, index: number): number 
  * Truncates text on the last space before the given index, and adds '...' at the end
  * @param {String} text Text to be truncated
  * @param {Number} maxLength Desired max length. Output might be shorter because it's truncated on the last space found before the given maxLength
+ * @param {Boolean} options.hideIfNoWords Hide the ellipsis if there are no words to show
  * @returns Truncated text
  */
 function truncateText(
