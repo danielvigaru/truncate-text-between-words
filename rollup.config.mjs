be1c04228c3e64ecbd907ba5d9075acaacc920b2
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2024-12-18 23:26:11 +0200

    chore: package with rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,16 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'lib/index.mjs',
+            format: 'es',
+        },
+        {
+            file: 'lib/index.cjs',
+            format: 'cjs',
+        },
+    ],
+    plugins: [typescript()],
+};
